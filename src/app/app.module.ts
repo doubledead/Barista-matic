2f414d970ce2fb337016719cc81165f6fd51c197
doubledead <jaa2015@gmail.com>
2023-01-18 16:27:43 -0600

    Remove unused inventory component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InventoryComponent } from './inventory/inventory.component';
 import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InventoryComponent,
     OrderComponent
   ],
   imports: [
