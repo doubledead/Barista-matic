be7b9f74cb505b05c45f682d41f193d69198f5bb
doubledead <jaa2015@gmail.com>
2023-01-23 12:54:55 -0600

    Hardcode JSON data.

--- src/app/services/ingredients/ingredient.service.ts
+++ src/app/services/ingredients/ingredient.service.ts
@@ -2,14 +2,80 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Ingredient } from '../../models/ingredient';
-import * as ingredientData from '../../../../api/ingredients.json'
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngredientService {
   url = 'http://localhost:3000';
-  jsonData = ingredientData;
+
+  data = {
+    "ingredients": [
+      {
+        "id": 1,
+        "cost": 0.75,
+        "name": "Coffee",
+        "outOfStock": false,
+        "stock": 10
+      },
+      {
+        "id": 2,
+        "cost": 0.75,
+        "name": "Decaf Coffee",
+        "outOfStock": false,
+        "stock": 10
+      },
+      {
+        "id": 3,
+        "cost": 0.25,
+        "name": "Sugar",
+        "outOfStock": false,
+        "stock": 10
+      },
+      {
+        "id": 4,
+        "cost": 0.25,
+        "name": "Cream",
+        "outOfStock": false,
+        "stock": 10
+      },
+      {
+        "id": 5,
+        "cost": 0.25,
+        "name": "Steamed Milk",
+        "outOfStock": false,
+        "stock": 10
+      },
+      {
+        "id": 6,
+        "cost": 0.35,
+        "name": "Foamed Milk",
+        "outOfStock": false,
+        "stock": 10
+      },
+      {
+        "id": 7,
+        "cost": 1.10,
+        "name": "Espresso",
+        "outOfStock": false,
+        "stock": 10
+      },
+      {
+        "id": 8,
+        "cost": 0.90,
+        "name": "Cocoa",
+        "outOfStock": false,
+        "stock": 10
+      },
+      {
+        "id": 9,
+        "cost": 1.00,
+        "name": "Whipped Cream",
+        "outOfStock": false,
+        "stock": 10
+      }
+    ]
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -18,6 +84,6 @@ export class IngredientService {
   }
 
   getIngredientsJSON(): Observable<any> {
-    return of(this.jsonData.ingredients);
+    return of(this.data.ingredients);
   }
 }
--- src/app/services/recipes/recipe.service.ts
+++ src/app/services/recipes/recipe.service.ts
@@ -2,14 +2,145 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Recipe } from 'src/app/models/recipe';
-import * as recipeData from '../../../../api/recipes.json'
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
   url = 'http://localhost:3000';
-  jsonData = recipeData;
+
+  data = {
+    "recipes": [
+      {
+        "id": 1,
+        "cost": 2.75,
+        "ingredients": [
+          {
+            "id": 1,
+            "name": "Coffee",
+            "stock": 3
+          },
+          {
+            "id": 3,
+            "name": "Sugar",
+            "stock": 1
+          },
+          {
+            "id": 4,
+            "name": "Cream",
+            "stock": 1
+          }
+        ],
+        "name": "Coffee",
+        "outOfStock": false
+      },
+      {
+        "id": 2,
+        "cost": 2.75,
+        "ingredients": [
+          {
+            "id": 2,
+            "name": "Decaf Coffee",
+            "stock": 3
+          },
+          {
+            "id": 3,
+            "name": "Sugar",
+            "stock": 1
+          },
+          {
+            "id": 4,
+            "name": "Cream",
+            "stock": 1
+          }
+        ],
+        "name": "Decaf Coffee",
+        "outOfStock": false
+      },
+      {
+        "id": 3,
+        "cost": 2.55,
+        "ingredients": [
+          {
+            "id": 7,
+            "name": "Espresso",
+            "stock": 2
+          },
+          {
+            "id": 5,
+            "name": "Steamed Milk",
+            "stock": 1
+          }
+        ],
+        "name": "Caffe Latte",
+        "outOfStock": false
+      },
+      {
+        "id": 4,
+        "cost": 3.30,
+        "ingredients": [
+          {
+            "id": 7,
+            "name": "Espresso",
+            "stock": 3
+          }
+        ],
+        "name": "Caffe Americano",
+        "outOfStock": false
+      },
+      {
+        "id": 5,
+        "cost": 3.35,
+        "ingredients": [
+          {
+            "id": 7,
+            "name": "Espresso",
+            "stock": 1
+          },
+          {
+            "id": 5,
+            "name": "Steamed Milk",
+            "stock": 1
+          },
+          {
+            "id": 8,
+            "name": "Cocoa",
+            "stock": 1
+          },
+          {
+            "id": 9,
+            "name": "Whipped Cream",
+            "stock": 1
+          }
+        ],
+        "name": "Caffe Mocha",
+        "outOfStock": false
+      },
+      {
+        "id": 6,
+        "cost": 2.90,
+        "ingredients": [
+          {
+            "id": 7,
+            "name": "Espresso",
+            "stock": 2
+          },
+          {
+            "id": 5,
+            "name": "Steamed Milk",
+            "stock": 1
+          },
+          {
+            "id": 6,
+            "name": "Foamed Milk",
+            "stock": 1
+          }
+        ],
+        "name": "Cappuccino",
+        "outOfStock": false
+      }
+    ]
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -18,6 +149,6 @@ export class RecipeService {
   }
 
   getIngredientsJSON(): Observable<any> {
-    return of(this.jsonData.recipes);
+    return of(this.data.recipes);
   }
 }
